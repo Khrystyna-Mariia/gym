d0b1a8ec0d9d81489cf6e9c8a09fa65ea2c14aa5
Khrystyna-Mariia Chomko <xrystynachomko@gmail.com>
2026-07-18 01:44:45 +0300

    setup embedded Tomcat, Spring MVC and Swagger OpenAPI configuration

--- src/main/java/org/gymcrm/Main.java
+++ src/main/java/org/gymcrm/Main.java
@@ -1,85 +1,70 @@
 package org.gymcrm;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
 import org.gymcrm.config.AppConfig;
-import org.gymcrm.context.UserContextHolder;
-import org.gymcrm.exception.AuthenticationException;
-import org.gymcrm.facade.GymFacade;
-import org.gymcrm.model.*;
+import org.gymcrm.config.WebConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import java.time.LocalDate;
+import java.io.File;
+import java.nio.file.Files;
+
+import static org.apache.tomcat.util.http.fileupload.FileUtils.deleteDirectory;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final int PORT = 8080;
 
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)) {
-            GymFacade gymCrmFacade = context.getBean(GymFacade.class);
+        try {
+            logger.info("Starting Embedded Tomcat server on port {}...", PORT);
 
-            logger.info("Registering new Trainee");
-            User userTrainee = new User();
-            userTrainee.setFirstName("John");
-            userTrainee.setLastName("Smith");
+            Tomcat tomcat = new Tomcat();
+            tomcat.setPort(PORT);
 
-            Trainee newTrainee = new Trainee();
-            newTrainee.setUser(userTrainee);
-            newTrainee.setDateOfBirth(LocalDate.of(2001, 3, 10));
-            newTrainee.setAddress("Ternopil");
+            File baseDir = Files.createTempDirectory("tomcat-base-dir").toFile();
+            tomcat.setBaseDir(baseDir.getAbsolutePath());
 
-            Trainee createdTrainee = gymCrmFacade.createTrainee(newTrainee);
+            tomcat.getConnector();
 
-            String traineeUsername = createdTrainee.getUser().getUsername();
-            String traineePassword = createdTrainee.getUser().getPassword();
-            logger.info("Successfully registered! Username: {}, Password: {}", traineeUsername, traineePassword);
+            String docBase = new File(".").getAbsolutePath();
+            Context context = tomcat.addContext("", docBase);
 
-            UserContextHolder.setCredentials(traineeUsername, traineePassword);
+            context.setParentClassLoader(Thread.currentThread().getContextClassLoader());
 
-            try {
-                logger.info("Accessing Protected Data (getAllTrainees)");
-                gymCrmFacade.getAllTrainees().forEach(t ->
-                        logger.info("Trainee found - Username: {}, Name: {} {}",
-                                t.getUser().getUsername(), t.getUser().getFirstName(), t.getUser().getLastName())
-                );
+            AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+            rootContext.register(AppConfig.class, WebConfig.class);
 
-                logger.info("Registering new Trainer");
-                User userTrainer = new User();
-                userTrainer.setFirstName("Alex");
-                userTrainer.setLastName("Brown");
+            DispatcherServlet dispatcherServlet = new DispatcherServlet(rootContext);
+            var dispatcherWrapper = Tomcat.addServlet(context, "dispatcher", dispatcherServlet);
+            dispatcherWrapper.setLoadOnStartup(1);
 
-                Trainer trainerModel = new Trainer();
-                trainerModel.setUser(userTrainer);
+            context.addServletMappingDecoded("/", "dispatcher");
 
-                TrainingType spec = new TrainingType();
-                spec.setId(1L);
-                trainerModel.setSpecialization(spec);
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    logger.info("Stopping Tomcat server...");
+                    tomcat.stop();
+                    tomcat.destroy();
 
-                Trainer createdTrainer = gymCrmFacade.createTrainer(trainerModel);
-                logger.info("Trainer registered with username: {}", createdTrainer.getUser().getUsername());
+                    deleteDirectory(baseDir);
+                    logger.info("Tomcat server stopped successfully.");
+                } catch (Exception e) {
+                    logger.error("Error while stopping Tomcat", e);
+                }
+            }));
 
-                logger.info("Creating Training Session (Protected)");
-                Training newTraining = new Training();
-                newTraining.setTrainee(createdTrainee);
-                newTraining.setTrainer(createdTrainer);
-                newTraining.setTrainingType(spec);
-                newTraining.setTrainingName("Personal Strength Training");
-                newTraining.setTrainingDate(LocalDate.of(2026, 6, 30));
-                newTraining.setTrainingDuration(90);
+            tomcat.start();
+            logger.info("Tomcat server successfully started!");
+            logger.info("Swagger UI is available at: http://localhost:{}/swagger-ui/index.html", PORT);
 
-                gymCrmFacade.createTraining(newTraining);
-                logger.info("Training session 'Personal Strength Training' created successfully!");
+            tomcat.getServer().await();
 
-            } finally {
-                UserContextHolder.clear();
-                logger.debug("Security context cleared from ThreadLocal.");
-            }
-
-            logger.info("Gym CRM Application finished successfully.");
-        } catch (AuthenticationException e) {
-            logger.warn("Security Violation: {}", e.getMessage());
         } catch (Exception e) {
-            logger.error("Application encountered an unexpected error during execution", e);
+            logger.error("Failed to start Embedded Tomcat server", e);
         }
     }
 }
--- src/main/java/org/gymcrm/config/AppConfig.java
+++ src/main/java/org/gymcrm/config/AppConfig.java
@@ -1,13 +1,12 @@
 package org.gymcrm.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("org.gymcrm")
+@ComponentScan(basePackages = "org.gymcrm",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = EnableWebMvc.class))
 @PropertySource("classpath:application.properties")
 public class AppConfig {
 
