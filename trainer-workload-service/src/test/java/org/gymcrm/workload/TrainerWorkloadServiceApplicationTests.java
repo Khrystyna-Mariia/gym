3900b0fc48a3ff4714e870735851e1b5845f3dc7
Khrystyna-Mariia Chomko <xrystynachomko@gmail.com>
2026-08-11 14:42:11 +0300

    Initialize trainer workload service project structure and configuration

---
+++ trainer-workload-service/src/test/java/org/gymcrm/workload/TrainerWorkloadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gymcrm.workload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerWorkloadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
